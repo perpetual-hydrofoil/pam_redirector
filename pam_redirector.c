88f7f469887275f26a72e72221f01748f66708c9
Jamieson Becker <jamieson@jamiesonbecker.com>
2013-04-03 16:32:48 -0500

    Added GPL License File

--- pam_redirector.c
+++ pam_redirector.c
@@ -4,15 +4,15 @@
  * By Jamieson Becker, 09 May 2007
  * Copyright (c) 2013, Jamieson Becker. GPL
  *
- * CREDITS
- * Benjamin Schieder for pam_extern on which this module is based
- * An unknown RPI professor for client.c and U_client.c
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version. A copy of the GNU General Public
  * License can be found at COPYING.
+ *
+ * CREDITS
+ * Benjamin Schieder for pam_extern on which this module is based
+ * An unknown RPI professor for client.c and U_client.c
  *
  * This PAM module is provides a simple client to a UNIX socket
  * server (running on localhost) to request the socket server to authenticate
